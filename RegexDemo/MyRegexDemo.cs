5b1731e3d5f71eb202e94ae5bc36fbbc2778e934
Gagalin-hash <58056756+Gagalin-hash@users.noreply.github.com>
2020-01-17 10:42:43 +0800

    2020-1-17

---
+++ RegexDemo/MyRegexDemo.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Text.RegularExpressions;
+using Newtonsoft.Json;
+namespace RegexDemo
+{
+    public class MyRegexDemo
+    {
+        public static void RegexMain()
+        {
+            string jsonText = @"{""input"" : ""value"", ""output"" : ""result""}";
+            JsonReader jrd = new JsonTextReader(new StringReader(jsonText));
+            while(jrd.Read()){
+                Type tmep_type=jrd.ValueType;
+               
+                string temp_str=Convert.ToString(jrd.Value);
+                Console.WriteLine(temp_str);
+            }
+            string regStr = @"\b" +
+                            @"(?'letter'\w)" +
+                            @"\w+" +
+                            @"\k'letter'" +
+                            @"\b";
+            foreach (Match m in Regex.Matches("bob pope peep wow hoh", regStr))
+                Console.WriteLine(m + " ");
+        }
+    }
+}
